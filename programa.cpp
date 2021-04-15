a321a276c7e57fef59d491b080a3aa54304e7268
hans2001 <hans20010907@gmail.com>
2021-04-16 06:59:54 +0800

    Create programa.cpp

---
+++ programa.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+cin>>int m>>int n;
+double grid[m][n];
+for(int i=0;i<m;i++){
+for(int j =0; j<n ;j++){
+cin>>grid[i][j];
+
+}
+
+}
+cin>>double K;
+int d = sqrt(K);
+matrix[d][d];
+for(int y=0;y<d,y++){
+for(int x =0;d<x++){
+cin>> matrix[y][x];
+}
+
+}
+cin>>double K;
+int d = sqrt(K);
+matrix[d][d];
+for(int y=0;y<d,y++){
+for(int x =0;d<x++){
+cin>> matrix[y][x];
+}
